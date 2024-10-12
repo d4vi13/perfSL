44b80f7a22e1cdcaee18519e13e39369da1f5383
dcr23 <dcr23@inf.ufpr.br>
2024-10-12 15:04:52 -0300

    gauss seidel and gaussian elimination implemented

--- src/perfSL.c
+++ src/perfSL.c
@@ -19,11 +19,10 @@ void print_ls(plinear_system ls){
 // TODO check mallocs 
 int  malloc_matrices(plinear_system sl){
     sl-> coeficientes = malloc(sl->ordem * sizeof * sl->coeficientes);
-    sl-> icognitas = malloc(sl->ordem * sizeof * sl->icognitas);
+    sl-> icognitas = calloc((sl->ordem), sizeof * sl->icognitas);
 
     for(int i = 0; i < sl->ordem; i++){
         sl-> coeficientes[i] = malloc((sl->ordem+1) * sizeof **sl->coeficientes);
-        sl-> icognitas[i] = malloc(sl->ordem * sizeof **sl->icognitas);
     }
 
     return 1;
@@ -39,14 +38,104 @@ int get_linear_system(plinear_system sl){
 
 }
 
+void retrosub(plinear_system ls){
+    for(int i = ls->ordem-1; i >= 0; i--){
+        ls->icognitas[i] = ls->coeficientes[i][ls->ordem];
+        for(int j = i+1; j < ls->ordem; j++)
+            /*
+             *  Here theres the following problems:
+             *  - When you subtract numbers it can create a lot of numeric imprecisions
+             *  - Theres also the possibility of an overflow due to the multiplication
+             * */
+            ls->icognitas[i] -= ls->coeficientes[i][j] * ls->icognitas[j];
+        // Heres theres the possibility of an division by zero
+        ls->icognitas[i] /= ls->coeficientes[i][i];
+    }  
+}
 
-void solve_with_eg(plinear_system ls){
+void partial_pivoting(plinear_system ls, int i){
+    int maxline_index = i; 
+    double max, *aux;
+    max = ls->coeficientes[i][i];
+    for(int j = i; j < ls->ordem; j++){
+        if(ls->coeficientes[j][i] > max){
+            max = ls->coeficientes[j][i];
+            maxline_index = j;
+        }
+    }
+    
+    aux = ls->coeficientes[i];
+    ls->coeficientes[i] = ls->coeficientes[maxline_index];
+    ls->coeficientes[maxline_index] = aux; 
     
 }
+
+
+void gaussian_elimination(plinear_system ls){
+    double m;
+    for(int i = 0; i < ls->ordem-1; i++){
+        partial_pivoting(ls, i);
+        for(int j = i+1; j < ls->ordem; j++){
+            /*
+             * notice that without the partial pivoting
+             * we may have a lot of errors, because if the pivot line
+             * has a small value, when we calculate m, we end up with a big m,
+             * and in floating point representation big numbers have a 
+             * very low precision, so when we do the partial pivoting, we
+             * end up with a smaller m, resulting in a more precise result
+             * */
+            m = ls->coeficientes[j][i]/ls->coeficientes[i][i];
+            ls->coeficientes[j][i] = 0.0;
+            for(int k = i+1; k <= ls->ordem; k++)
+                ls->coeficientes[j][k] -= ls->coeficientes[i][k]*m; 
+        }
+    } 
+}
+
+double sum(plinear_system ls, int i){
+    double total = ls->coeficientes[i][ls->ordem];
+    for(int j = 0; j < i; j++)
+        total -= ls->icognitas[j]*ls->coeficientes[i][j]; 
+    for(int j = i+1; j < ls->ordem; j++)
+        total -= ls->icognitas[j]*ls->coeficientes[i][j]; 
+    total /= ls->coeficientes[i][i];
+    
+    printf("total: %.15e\n",total);
+    return total;
+}
+
+double max_error(plinear_system ls, double *x){
+    double prev, max = fabs(ls->icognitas[0] - x[0]);
+    for(int i = 0; i < ls->ordem; i++){
+       prev = fabs(ls->icognitas[i] - x[i]);
+       if (prev < max)
+          max = prev; 
+    }
+    return max;
+}
+
+void gauss_seidel(plinear_system ls){
+    double* x = calloc(ls->ordem, sizeof * x);
+   
+    do{ 
+        memcpy(x, ls->icognitas, ls->ordem*sizeof*x);
+        for(int i = 0; i < ls->ordem; i++){
+            ls->icognitas[i] = sum(ls, i);
+            printf("ic: %.15e\n", ls->icognitas[i]);
+        }
+    }while(max_error(ls, x) > 0.0001);
+}
+
 
 int main(){
     linear_system sl;
     get_linear_system(&sl);
+    //gaussian_elimination(&sl);
+    //retrosub(&sl);
+    gauss_seidel(&sl);
+    for (int i = 0; i < sl.ordem; i++)
+        printf("%.15e ", sl.icognitas[i]);
+    printf("\n"); 
     print_ls(&sl);
 }
 
--- src/perfSL.h
+++ src/perfSL.h
@@ -3,10 +3,12 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <math.h>
 
 struct linear_system{
     double** coeficientes;
-    double** icognitas;
+    double* icognitas;
     int ordem;
 };
 
